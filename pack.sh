6a3b3769cbeb9436878c5dc2531bd98df1f2dc1a
esrrhs <esrrhs@163.com>
2021-04-19 20:20:44 +0800

    add

--- pack.sh
+++ pack.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 set -x
+NAME=go-cpuminer
 
 CGO_ENABLED=0 go build
-zip $NAME_linux64.zip $NAME
+zip $(NAME)_linux64.zip $NAME
 
 GOOS=darwin GOARCH=amd64 go build
-zip $NAME_mac.zip $NAME
+zip $(NAME)_mac.zip $NAME
 
 GOOS=windows GOARCH=amd64 go build
-zip $NAME_windows64.zip $NAME.exe
+zip $(NAME)_windows64.zip $NAME.exe
 
 GOOS=linux GOARCH=mipsle go build
-zip $NAME_mipsle.zip $NAME
+zip $(NAME)_mipsle.zip $NAME
 
 GOOS=linux GOARCH=arm go build
-zip $NAME_arm.zip $NAME
+zip $(NAME)_arm.zip $NAME
 
 GOOS=linux GOARCH=mips go build
-zip $NAME_mips.zip $NAME
+zip $(NAME)_mips.zip $NAME
 
 GOOS=windows GOARCH=386 go build
-zip $NAME_windows32.zip $NAME.exe
+zip $(NAME)_windows32.zip $NAME.exe
 
 GOOS=linux GOARCH=arm64 go build
-zip $NAME_arm64.zip $NAME
+zip $(NAME)_arm64.zip $NAME
 
 GOOS=linux GOARCH=mips64 go build
-zip $NAME_mips64.zip $NAME
+zip $(NAME)_mips64.zip $NAME
 
 GOOS=linux GOARCH=mips64le go build
-zip $NAME_mips64le.zip $NAME
-
+zip $(NAME)_mips64le.zip $NAME
