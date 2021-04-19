b7b12876b1051cbebe384eb2b234d2c56175bdf9
esrrhs <esrrhs@163.com>
2021-04-19 20:37:05 +0800

    add

--- pack.sh
+++ pack.sh
@@ -3,33 +3,12 @@ set -x
 NAME="go-cpuminer"
 rm *.zip -f
 
-GOOS=linux GOARCH=amd64 go build
-zip $NAME"_linux64.zip" $NAME
+os_all='linux windows darwin freebsd'
+arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle'
 
-GOOS=darwin GOARCH=amd64 go build
-zip $NAME"_mac.zip" $NAME
-
-GOOS=windows GOARCH=amd64 go build
-zip $NAME"_windows64.zip" $NAME.exe
-
-GOOS=linux GOARCH=mipsle go build
-zip $NAME"_mipsle.zip" $NAME
-
-GOOS=linux GOARCH=arm go build
-zip $NAME"_arm.zip" $NAME
-
-GOOS=linux GOARCH=mips go build
-zip $NAME"_mips.zip" $NAME
-
-GOOS=windows GOARCH=386 go build
-zip $NAME"_windows32.zip" $NAME.exe
-
-GOOS=linux GOARCH=arm64 go build
-zip $NAME"_arm64.zip" $NAME
-
-GOOS=linux GOARCH=mips64 go build
-zip $NAME"_mips64.zip" $NAME
-
-GOOS=linux GOARCH=mips64le go build
-zip $NAME"_mips64le.zip" $NAME
-
+for os in $os_all; do
+  for arch in $arch_all; do
+    GOOS=$os GOARCH=$arch go build
+    zip ${NAME}_${os}_${arch}".zip" $NAME
+  done
+done
