ec5b64db7c453ea4dbc3d54a1353c68f5124f03a
esrrhs <esrrhs@163.com>
2021-04-20 09:58:41 -0400

    add

--- pack.sh
+++ pack.sh
@@ -52,7 +52,7 @@ for line in $build_list; do
   if [ $os == "android" ]; then
     continue
   fi
-  CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build
+  CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w"
   if [ $? -ne 0 ]; then
     echo "os="$os" arch="$arch" build fail"
     exit 1
